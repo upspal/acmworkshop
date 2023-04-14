b5e5b04a2cf58e22f70d212f0e13d1cbda68a789
upspal <uppal.pranav@gmail.com>
2023-04-15 01:21:28 +0530

    homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
+import '../style/Home.css'
+import image from '../assets/dashboard.png';
 
 export class Home extends Component {
   render() {
-    return (
-      <div>Home</div>
+    return (   
+      <div className='home_main_container'>Home
+        <div className='home_button'>
+        <p>Just Launched Ghost 3.0</p>    
+        </div>      
+        <div className='home_heading'>
+        <p>Turn your audience<br/> <span className= 'business'>into a business</span></p>    
+        </div>
+        <div className='home_background'></div>      
+        <div className='home_image'><img src={image}/></div>      
+      </div>
     )
   }
 }
